1ded87a0aa07bda630598e979624c452f444a212
nellie <chanelcarp@gmail.com>
2022-11-06 14:58:11 -0800

    add user model and registration capability

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const {Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+    {
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        }
+    }, 
+    {
+        timestamps: true
+    }
+);
+
+const User = model("user", UserSchema);
+module.exports = User;
