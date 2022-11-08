2279c42e306ae4ef3976c6ba28cf929a9ea765d8
nellie <chanelcarp@gmail.com>
2022-11-08 09:48:52 -0800

    set up user authorization

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const validateRegisterInput = require("../validation/registerValidation");
+const jwt = require("jsonwebtoken");
+const requiresAuth = require("../middleware/permissions");
 
 // @route   GET /api/auth/test
 // @desc    Test the auth route
@@ -45,12 +47,73 @@ router.post("/register", async(req, res) => {
         // save user to db
         const savedUser = await newUser.save();
 
+        const userToReturn = {...savedUser._doc }
+        delete userToReturn.password;
+
         // return new user
-        return res.json(savedUser);
+        return res.json(userToReturn);
     } catch (err) {
         console.log(err);
         res.status(500).send(err.message);
     }
-})
+});
+
+// @route   POST /api/auth/login
+// @desc    Login user and return access token
+// @access  Public
+router.post("/login", async(req, res) => {
+    try{
+        // check for the user
+        const user = await User.findOne({
+            email: new RegExp("^" + req.body.email + "$", "i")
+        })
+
+        if(!user) {
+            return res.status(400).json({error: "invalid login credentials"})
+        }
+
+        const passwordMatch = await bcrypt.compare(req.body.password, user.password);
+
+        if(!passwordMatch) {
+            return res.status(400).json({error: "invalid login credentials"})
+        }
+
+        const payload = { userId: user._id };
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: "7d"
+        });
+
+        res.cookie("access-token", token, {
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+            httpOnly: true,
+            // use secure tokens when in production, but not when testing locally
+            secure: process.env.NODE_ENV === "production"
+        });
+
+        const userToReturn = { ...user._doc };
+        delete userToReturn.password;
+
+        return res.json({
+            token: token,
+            user: userToReturn
+        });
+    } catch(err) {
+        console.log(err);
+
+        return res.status(500).send(err.message);
+    }
+});
+
+// @route   GET /api/auth/current
+// @desc    Return the currently authed user
+// @access  Private
+router.get("/current", requiresAuth, (req, res) => {
+    if(!req.user) {
+        return res.status(401).send("unauthorized");
+    }
+
+    return res.json(req.user);
+});
 
 module.exports = router;
