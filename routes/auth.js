ee529bc8d1fe51482afd1bac85591f32742e1c68
nellie <chanelcarp@gmail.com>
2022-11-08 07:23:02 -0800

    add user validation to registration

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
+const validateRegisterInput = require("../validation/registerValidation");
 
 // @route   GET /api/auth/test
 // @desc    Test the auth route
@@ -15,6 +16,22 @@ router.get("/test", (req, res) => {
 // @access  Public
 router.post("/register", async(req, res) => {
     try{
+        const { errors, isValid } = validateRegisterInput(req.body);
+
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
+
+
+        // check for existing user
+        const existingEmail = await User.findOne({
+            email: new RegExp("^" + req.body.email + "$", "i")
+        });
+
+        if(existingEmail) {
+            return res.status(400).json({error: 'There is already a user with this email'})
+        }
+
         // hash password
         const hashedPassword = await bcrypt.hash(req.body.password, 12);
 
---
+++ validation/registerValidation.js
@@ -0,0 +1,41 @@
+const Validator = require('validator');
+const isEmpty = require("./isEmpty");
+
+const validateRegisterInput = (data) => {
+    let errors = {};
+
+    // check email field
+    if(isEmpty(data.email)) {
+        errors.email = "email field cannot be empty";
+    } else if(!Validator.isEmail(data.email)) {
+        errors.email = 'email is invalid, please provide a valid email';
+    }
+
+    // check name field
+    if(isEmpty(data.name)) {
+        errors.name = "name cannot be empty"
+    } else if(!Validator.isLength(data.name, { min: 2, max: 30 })){
+        errors.name = "name must be between 2 and 30 characters long";
+    }
+
+    // check password field
+    if(isEmpty(data.password)){
+        errors.password = "password field cannot be empty";
+    } else if(!Validator.isLength(data.password, { min: 6, max: 150 })) {
+        errors.password = "password must be between 6 and 150 characters long";
+    }
+
+    // check confirm password field
+    if(isEmpty(data.confirmPassword)) {
+        errors.confirmPassword = "confirm password field cannot be empty";
+    } else if(!Validator.equals(data.password, data.confirmPassword)) {
+        errors.confirmPassword = "password fields must match"
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors),
+    }
+};
+
+module.exports = validateRegisterInput;
